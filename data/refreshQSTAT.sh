13c618a1390c285417eef654c1db36370e36cb0f
hnieto <hnieto@tacc.utexas.edu>
2012-11-02 11:16:47 -0500

    bash script now takes user as argument

--- data/refreshQSTAT.sh
+++ data/refreshQSTAT.sh
@@ -1,32 +1,31 @@
 #!/bin/sh
 
 # Name: refreshQSTAT.sh
-# Description: run " qstat -u '*' " on supercomputers and copy file over to local processing /data folder# Developer: Heriberto Nieto
+# Description: run " qstat -u '*' " on supercomputers and copy file over to local processing /data folder
+# Developer: Heriberto Nieto
 
+scriptName=`basename $0`
+user=$1
 host=ranger.tacc.utexas.edu
-user=hnieto
 File=rangerQSTAT.xml
 longFile=rangerQSTAT-long.xml
 shortFile=rangerQSTAT-short.xml
 processingDataDir=$(pwd)
 
+if [ $# -eq 0 ]
+  then
+    echo "Usage: ./$scriptName user"
+    exit 1
+fi
 
 echo "Log into $host as $user and update $File .........."
-ssh $user@$host "rm $File; qstat -u '*' -xml > $File; stat $File | grep Change | awk -F"." '{ print \$1 }' | awk -v "FILE=$File" '{ print FILE \" updated on \" \$2 \" \" \$3 }' "
-echo "###################################################"
-
-echo "\n"
-echo "Remove local copy of $longFile .........."
-rm $processingDataDir/$longFile
+ssh $user@$host "qstat -u '*' -xml > $File; stat $File | grep Change | awk -F"." '{ print \$1 }' | awk -v "FILE=$File" '{ print FILE \" updated on \" \$2 \" \" \$3 }' "
+echo "#############################################################################################\n"
 
 echo "Copy $File from $host and save as $longFile .........."
-scp $user@$host:/share/home/02117/$user/$File $processingDataDir/$longFile 
-stat $processingDataDir/$longFile | awk -v "FILE=$longFile" -F"\"" '{ print FILE " updated on " $2 }' 
-echo "###################################################"
-
-echo "\n" 
-echo "Remove local copy of $shortFile"
-rm $processingDataDir/$shortFile
+scp $user@$host:~/$File $processingDataDir/$longFile 
+stat -f "%Sm" $processingDataDir/$longFile | awk -v "FILE=$longFile" '{ print FILE " updated on " $0 }' 
+echo "#############################################################################################\n"
 
 echo "Make a shorter version of $longFile, $shortFile, for debugging purposes"
 head -n 1003 $processingDataDir/$longFile > $processingDataDir/$shortFile
@@ -34,4 +33,4 @@ head -n 1003 $processingDataDir/$longFile > $processingDataDir/$shortFile
 # close xml tags
 echo " </queue_info>" >> $processingDataDir/$shortFile
 echo "</job_info>" >> $processingDataDir/$shortFile
-stat $processingDataDir/$shortFile | awk -v "FILE=$shortFile" -F"\"" '{ print FILE " updated on " $2 }'
+stat -f "%Sm" $processingDataDir/$shortFile | awk -v "FILE=$shortFile" '{ print FILE " updated on " $0 }'
